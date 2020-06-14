720c1f9879f9c629ec44cf9873e1e152073ab702
Jiaqi Zhang <zhangjiaqi_seu@outlook.com>
2020-06-14 21:26:44 +0800

    Update MultiAgentAnalysis.py

--- Utility_Tree_Analysis/MultiAgentAnalysis.py
+++ Utility_Tree_Analysis/MultiAgentAnalysis.py
@@ -14,6 +14,8 @@ import pandas as pd
 import pymc3 as pm
 from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso
 from sklearn.naive_bayes import MultinomialNB, CategoricalNB, GaussianNB
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 
@@ -45,6 +47,7 @@ class MultiAgentAnalysis:
             lambda x: np.argmax([float(each) for each in x.strip('[]').split(' ')]) if not isinstance(x, float) else -1
         )
         print()
+
 
     def _constructDataset(self):
         X = np.vstack(
@@ -94,17 +97,25 @@ class MultiAgentAnalysis:
         '''
         # Construct dataset
         X, _, Y = self._constructDataset()
+        # Split training and testing set
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
         # Linear regression
         if need_reg:
             model = Lasso(alpha = 1, fit_intercept = False)
         else:
             model = LinearRegression(fit_intercept=False)
-        model.fit(X, Y)
+        model.fit(X_train, Y_train)
+        Y_pred = model.predict(X_test)
+        Y_pred = np.round(Y_pred)
+        Y_pred[np.where(Y_pred < 0 )] = 0
+        Y_pred[np.where(Y_pred > 3)] = 3
+        correct_rate = np.sum(Y_pred == Y_test) / len(Y_pred)
         # The coefficient
         LR_coeff = model.coef_
         # LR_coeff = np.array([np.sum(np.abs(LR_coeff)[i:i + 2]) for i in [0, 2, 4, 6]])
         LR_coeff = np.abs(LR_coeff) / np.sum(np.abs(LR_coeff))
         print("{} Coefficient: {}".format("Lasso" if need_reg else "Linear Regression", LR_coeff))
+        print("Correct Rate on Testing Set: {}".format(correct_rate))
 
 
     def MultinomialNBAnalysis(self):
@@ -151,14 +162,93 @@ class MultiAgentAnalysis:
             # print(pm.fit())
 
 
+    def movingWindowAnalysis(self, window = 100):
+        '''
+        Show the 
+        :param window: The length of the window
+        :return: 
+        '''
+        # Construct dataset
+        X, _, Y = self._constructDataset()
+        # The subset
+        subset_index = np.arange(window, len(Y)-window)
+        # Linear regression
+        all_coeff = []
+        all_correct_rate = []
+        for index in subset_index:
+            sub_X = X[index-window:index+window, :]
+            sub_Y = Y[index-window:index+window]
+            X_train, X_test, Y_train, Y_test = train_test_split(sub_X, sub_Y, test_size = 0.2)
+            model = LinearRegression(fit_intercept = False)
+            model.fit(X_train, Y_train)
+            # Correct rate
+            Y_pred = model.predict(X_test)
+            Y_pred = np.round(Y_pred)
+            Y_pred[np.where(Y_pred < 0)] = 0
+            Y_pred[np.where(Y_pred > 3)] = 3
+            correct_rate = np.sum(Y_pred == Y_test) / len(Y_pred)
+            all_correct_rate.append(correct_rate)
+            # The coefficient
+            LR_coeff = model.coef_
+            # LR_coeff = np.array([np.sum(np.abs(LR_coeff)[i:i + 2]) for i in [0, 2, 4, 6]])
+            LR_coeff = np.abs(LR_coeff) / np.sum(np.abs(LR_coeff))
+            all_coeff.append(LR_coeff)
+        print("Average Coefficient: {}".format(np.mean(all_coeff, axis = 0)))
+        print("Average Correct Rate: {}".format(np.mean(all_correct_rate)))
+        # Plot weight variation
+        all_coeff = np.array(all_coeff)
+        plt.stackplot(np.arange(all_coeff.shape[0]),
+                      all_coeff[:, 3], # random agent
+                      all_coeff[:, 2], # lazy agent
+                      all_coeff[:, 1], # local agent
+                      all_coeff[:, 0], # global agent
+                      labels = ["Random Agent", "Lazy Agent", "Local Agent", "Global Agent"])
+        plt.ylim(0, 1.0)
+        plt.ylabel("Agent Percentage (%)", fontsize = 20)
+        plt.yticks(fontsize = 20)
+        plt.xlim(0, all_coeff.shape[0])
+        plt.xlabel("Time Step", fontsize = 20)
+        plt.xticks(fontsize = 20)
+        plt.legend(fontsize = 20)
+        plt.show()
+
+        plt.clf()
+        plt.plot(all_coeff[:, 0], "o-", label = "Global Agent", ms = 2, lw = 0.5)
+        plt.plot(all_coeff[:, 1], "o-", label = "Local Agent", ms = 2, lw = 0.5)
+        plt.plot(all_coeff[: ,2], "o--",label = "Lazy Agent", ms = 2, lw = 0.5)
+        plt.plot(all_coeff[:, 3], "o--", label = "Random Agent", ms = 2, lw = 0.5)
+        plt.ylabel("Agent Weight ($\\beta$)", fontsize = 20)
+        plt.yticks(fontsize = 20)
+        plt.xlim(0, all_coeff.shape[0])
+        plt.xlabel("Time Step", fontsize = 20)
+        plt.xticks(fontsize = 20)
+        plt.legend(fontsize = 20)
+        plt.show()
+
+        plt.clf()
+        plt.title("Correct Rate vs. Time Step", fontsize = 20)
+        plt.plot(all_correct_rate, "d-", label="Correct Rate", ms=3, lw=2)
+        plt.ylabel("Correct Rate (%)", fontsize=20)
+        plt.ylim(0, 1.1)
+        plt.yticks(fontsize=20)
+        plt.xlim(0, len(all_correct_rate))
+        plt.xlabel("Time Step", fontsize=20)
+        plt.xticks(fontsize=20)
+        # plt.legend(fontsize=20)
+        plt.show()
 
 
 if __name__ == '__main__':
-    filename = "stimulus_data/global-graze/diary.csv"
+    filename = "stimulus_data/complex-condition/stimulus-global-local.csv"
+    # filename = "stimulus_data/local-graze/diary.csv"
+
     analysis = MultiAgentAnalysis(filename)
     #
-    analysis.LogisticAnalysis()
-    analysis.LinearAnalysis(need_reg=False)
+    # analysis.LogisticAnalysis()
+    # analysis.LinearAnalysis(need_reg=False)
     # analysis.BayesianAnalysis()
-    analysis.MultinomialNBAnalysis()
+    # analysis.MultinomialNBAnalysis()
     # analysis.CategoricalNBAnalysis()
+
+    # 20 is enough for global graze
+    analysis.movingWindowAnalysis(window = 200)
